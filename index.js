ad3eb6f152f76d5135e981b0a4cfc3f822f67e6d
Teemu Paananen <Teemu.Paananen@edu.savonia.fi>
2023-01-03 11:10:06 +0200

    hello world

---
+++ index.js
@@ -0,0 +1,10 @@
+/* eslint-disable no-undef */
+const express = require("express");
+const app = express();
+const port = 8080;
+
+app.use(express.static("public"));
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
