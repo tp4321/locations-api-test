2a19d76e66c389876143b9c87379846a027b5f69
Teemu Paananen <Teemu.Paananen@edu.savonia.fi>
2023-01-03 11:26:53 +0200

    numbers

--- index.js
+++ index.js
@@ -4,6 +4,18 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(express.static("public"));
+app.get("/randomize", (req, res) => {
+  var number1 = Math.random() * 3;
+  var number2 = Math.random() * 3;
+  var number3 = Math.random() * 3;
+
+  res.send("number1: " + number1);
+  res.send("number2: " + number2);
+  res.send("number3: " + number3);
+  if (number1 == number2 && number1 == number3) {
+    res.send("success!");
+  }
+});
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
